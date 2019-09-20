6e0cec4ee7323e5f3da32b0855099a6a2262890e
Zack Chestler <zack.chestler@nwea.org>
2019-09-20 15:03:40 -0700

    edited file to run long term

--- pi-pic-script/pic-script.py
+++ pi-pic-script/pic-script.py
@@ -20,29 +20,35 @@ city_name = 'SunriverOR'
 # create astral location object for sunriver Oregon
 l = astral.Location((city_name, 'USA', 43.8694, -121.4334, 'US/Pacific', 4164)) # name, region, lat, long, timezone, elevation
 
-dateToday = datetime.date.today() # get todays date
+while 1:
+    dateToday = datetime.date.today() # get todays date
 
-sunInfo = l.sun(dateToday, local=True) # Get Sun information for Location
+    sunInfo = l.sun(dateToday, local=True) # Get Sun information for Location
 
-print('Dawn:    %s' % str(sunInfo['dawn']))
-print('Dusk:    %s' % str(sunInfo['dusk']))
+    print('Dawn:    %s' % str(sunInfo['dawn']))
+    print('Dusk:    %s' % str(sunInfo['dusk']))
 
-pictures = 0
-now = pst.localize(datetime.datetime.now()) # get the time of now
-
-while now < sunInfo['dusk']: # While it is earlier than dusk repeat
-    if now < sunInfo['dawn']:
-        print('sun has not risen %s' % str(now))
-    else:
-        print('sun is up take a photo %s picture: %s' % (str(now), str(pictures)))
-        timeStamp = strftime("%Y-%m-%d_%X")     # YYYY-mm-dd_time
-        fileName = '8_towhee_' + timeStamp + '.jpg'
-        camera.annotate_text = fileName
-        camera.capture(fileName)
-        bucket.upload_file(fileName, fileName)
-        os.remove(fileName)
-        pictures = pictures + 1
-    sleep(900)
+    pictures = 0
     now = pst.localize(datetime.datetime.now()) # get the time of now
 
-print('Finished Taking pictures for the day sun has gone. Total pictues: %s' % str(pictures))
+    while now < sunInfo['dusk']: # While it is earlier than dusk repeat
+        if now < sunInfo['dawn']:
+            print('sun has not risen %s' % str(now))
+        else:
+            print('sun is up take a photo %s picture: %s' % (str(now), str(pictures)))
+            timeStamp = strftime("%Y-%m-%d_%X")     # YYYY-mm-dd_time
+            fileName = '8_towhee_' + timeStamp + '.jpg'
+            camera.annotate_text = fileName
+            camera.capture(fileName)
+            bucket.upload_file(fileName, fileName)
+            os.remove(fileName)
+            pictures = pictures + 1
+        sleep(900)
+        now = pst.localize(datetime.datetime.now()) # get the time of now
+
+    print('Finished Taking pictures for the day sun has gone. Total pictues: %s' % str(pictures))
+
+    tomorrowCheck = datetime.date.today() # get todays date
+    while dateToday == tomorrowCheck:
+        sleep(900)
+        tomorrowCheck = datetime.date.today()

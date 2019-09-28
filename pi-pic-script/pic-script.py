82778bc6363fd88391c292ebc7ce3593014540bd
Zack Chestler <zack.chestler@nwea.org>
2019-09-28 16:23:10 -0700

    remove need for module

--- pi-pic-script/pic-script.py
+++ pi-pic-script/pic-script.py
@@ -5,7 +5,16 @@ from picamera import PiCamera
 import datetime
 import pytz
 from utils.s3_utils import clearBucket
-from utils.time_utils.py import getDayLight
+import astral
+
+
+
+def getDayLight(date):
+    city_name = 'SunriverOR'
+    # create astral location object for sunriver Oregon
+    l = astral.Location((city_name, 'USA', 43.8694, -121.4334, 'US/Pacific', 4164)) # name, region, lat, long, timezone, elevation
+    l.sun(date, local=True) # Get Sun information for Location
+    return l
 
 pst=pytz.timezone('US/Pacific')
 
